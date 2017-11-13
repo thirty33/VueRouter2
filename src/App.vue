be0f5589d1693f2190747d1619f9802c2aedfd94
Thirty33 <joelsuarez.1101@gmail.com>
2017-11-12 19:24:29 -0500

    transitions en componentes con vue router

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <!-- <img src="./assets/logo.png"> -->
     <button @click="cambiar">Cambiar auth</button>
     <hr>
-    <router-view/>
+    <!-- transiitons en componentes -->
+    <transition name="aparecer" appear>
+      
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -24,4 +28,51 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+
+
+<style>
+  
+  .completado {
+      text-decoration: line-through;
+    }
+    /*Entrada de la animation*/
+    .aparecer-enter {
+      opacity:0;
+    }
+    /*Transcurso de la entrada*/
+    .aparecer-enter-active{
+      transition: opacity 1s;
+    }
+
+    /*salida de la animacion*/
+    .aparecer-leave-to{
+      opacity: 0;
+    }
+
+    /*Transcurso de la salida*/
+    .aparecer-leave-active{
+      transition: opacity 1s; 
+    }
+
+    /*animacion bounce de ejemplo*/
+
+    .bounce-enter-active {
+      animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+      animation: bounce-in .5s reverse;
+    }
+
+    @keyframes bounce-in {
+      0% {
+        transform: scale(0);
+      }
+      50% {
+        transform: scale(1.5);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }
 </style>
